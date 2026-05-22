132e7d1c9c34cdc8c12ffbb7eba38d40c72ad8a6
DIEGO RODRIGUES JOAQUIM <diego.rjoaquim@senacsp.edu.br>
2026-05-22 17:24:11 -0300

    mobile

--- App.js
+++
@@ -1,9 +0,0 @@
-import { NavigationContainer } from "@react-navigation/native";
-import AppRoutes from './src/routes/AppRoutes';
-export default function App(){
-    return(
-      <NavigationContainer>
-        <AppRoutes />
-      </NavigationContainer>
-    )
-}
